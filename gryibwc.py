43a4766ef82190428c7defa50cd5b5b158d9401e
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-12-20 18:23:21 +0100

    Scrape word count

--- gryibwc.py
+++ gryibwc.py
@@ -11,7 +11,24 @@ import argparse
 import configparser
 import goodreads_api_client as gr
 import dateutil.parser
+import requests
+from bs4 import BeautifulSoup
 
+
+def get_word_count(isbn):
+    url = "https://www.readinglength.com/book/isbn-" + isbn
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    text = soup.get_text()
+    if "wordCount" in text:
+        print("Found stirng")
+        pos = text.rfind("wordCount")
+        wc_str = text[pos:pos+18]
+        com_pos = wc_str.find(',')
+        col_pos = wc_str.find(':')
+        wc = int(wc_str[col_pos+1:com_pos])
+        print(wc)
+    return wc
 
 def main(args):
     config = configparser.ConfigParser()
@@ -19,20 +36,23 @@ def main(args):
     key = config['api']['key']
 
     client = gr.Client(developer_key=key)
-    result = client.Review.list(args.userid, name="read", sort="date_read",
-                                per_page=200)
-    reviews = result['reviews']['review']
+    # result = client.Review.list(args.userid, name="read", sort="date_read",
+                                # per_page=200)
+    # reviews = result['reviews']['review']
 
-    for review in reviews:
+    # for review in reviews:
         # Since output is sorted from recently read to older,
         # break when the first book of the previous year is reached
-        year_read = int(dateutil.parser.parse(review['read_at']).year)
-        if year_read < args.year:
-            break
-        print(review['book']['title'])
-        print(review['book']['isbn'])
-        print(review['read_at'])
-        print(year_read)
+        # year_read = int(dateutil.parser.parse(review['read_at']).year)
+        # if year_read < args.year:
+            # break
+        # print(review['book']['title'])
+        # print(review['book']['isbn'])
+        # print(review['read_at'])
+        # print(year_read)
+
+    word_count = get_word_count("0765326361")
+    print(word_count)
 
 
 if __name__ == "__main__":
